be3d0febcac249e849292201912bf73f05addc55
root <root@ip-172-31-37-114.ec2.internal>
2023-03-13 15:32:46 +0000

    Adicionando IGW e associando

--- network.tf
+++ network.tf
@@ -57,7 +57,30 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+#Criando Route Table
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vorx-vpc-prod.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-public-rt"
+  }
+}
+
+#Associando Subnet a RT
+resource "aws_route_table_association" "pub-rt-1a" {
+  subnet_id      = aws_subnet.vorx-subnet-pub-1a.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "pub-rt-1b" {
+  subnet_id      = aws_subnet.vorx-subnet-pub-1b.id
+  route_table_id = aws_route_table.public-rt.id
+}
 
 
 ## OUTPUT ##
